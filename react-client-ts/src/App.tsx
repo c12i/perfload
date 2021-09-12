e691e4947d379acbf82461d5c56c11ffe38fa070
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-09-12 22:57:31 +0300

    Update system information description

--- react-client-ts/src/App.tsx
+++ react-client-ts/src/App.tsx
@@ -4,30 +4,32 @@ import SystemInfoCard from './components/SystemInfoCard'
 import useSocket from './hooks/useSocket'
 
 const App = () => {
-  const socket = useSocket()
-  const [performanceData, setPerformanceData] = useState(null)
-  useEffect(() => {
-    socket.on('data', (data) => {
-      const currentState = { ...performanceData }
-      currentState[data.macAddress] = data
-      setPerformanceData(currentState)
-    })
-  }, [])
-  console.log(performanceData)
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      {!!performanceData &&
-        Object.entries(performanceData).map(([macAddress, data]) => (
-          <SystemInfoCard data={data} key={macAddress} />
-        ))}
-    </div>
-  )
+    const socket = useSocket()
+    const [performanceData, setPerformanceData] = useState(null)
+    useEffect(() => {
+        socket.on('data', (data) => {
+            const currentState = { ...performanceData }
+            currentState[data.macAddress] = data
+            setPerformanceData(currentState)
+        })
+    }, [])
+    console.log(performanceData)
+    return (
+        <div
+            style={{
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                marginBottom: 10,
+            }}
+        >
+            {!!performanceData &&
+                Object.entries(performanceData).map(([macAddress, data]) => (
+                    <SystemInfoCard data={data} key={macAddress} />
+                ))}
+        </div>
+    )
 }
 
 export default App
--- react-client-ts/src/components/SystemInfoCard.tsx
+++ react-client-ts/src/components/SystemInfoCard.tsx
@@ -1,26 +1,48 @@
 import { Badge, Card, Descriptions } from 'antd'
+import dayjs from 'dayjs'
+import Duration from 'dayjs/plugin/duration'
+import RelativeTime from 'dayjs/plugin/relativeTime'
 import Gauge from './CustomGauge'
 
+dayjs.extend(Duration)
+dayjs.extend(RelativeTime)
+
 interface Props {
-  data?: any
+    data?: any
 }
 
-const Widget = ({ data }: Props) => {
-  return (
-    <Badge.Ribbon text={`${data?.osType} Machine`}>
-      <Card style={{ width: 900 }}>
-        <Card.Grid hoverable={false} style={{ width: '40%' }}>
-          <h3>CPU Load</h3>
-          <Gauge percent={data?.cpuLoad} />
-          <Descriptions title="CPU Info" layout="vertical">
-            <Descriptions.Item label="Model">{data?.cpuModel}</Descriptions.Item>
-            <Descriptions.Item label="Speed">{data?.cpuSpeed}</Descriptions.Item>
-            <Descriptions.Item label="Cores">{data?.cpuCoreCount}</Descriptions.Item>
-          </Descriptions>
-        </Card.Grid>
-      </Card>
-    </Badge.Ribbon>
-  )
+const Widget: React.FC<Props> = ({ data }) => {
+    return (
+        <Badge.Ribbon text={`${data?.osType} Machine`}>
+            <Card style={{ width: 900 }}>
+                <Card.Grid hoverable={false} style={{ width: '40%' }}>
+                    <h3>CPU Load</h3>
+                    <Gauge percent={data?.cpuLoad} />
+                    <SystemDescriptions data={data} />
+                </Card.Grid>
+            </Card>
+        </Badge.Ribbon>
+    )
 }
+
+const SystemDescriptions: React.FC<Props> = ({ data }) => (
+    <Descriptions title="CPU Info" layout="vertical">
+        <Descriptions.Item label={<strong>CPU Model</strong>}>
+            {data?.cpuModel}
+        </Descriptions.Item>
+        <Descriptions.Item label={<strong>Speed</strong>}>
+            {data?.cpuSpeed}
+        </Descriptions.Item>
+        <Descriptions.Item label={<strong>Cores</strong>}>
+            {data?.cpuCoreCount}
+        </Descriptions.Item>
+        <Descriptions.Item label={<strong>Uptime</strong>}>
+            {dayjs.duration(data?.uptime, 'seconds').humanize()}
+        </Descriptions.Item>
+        <Descriptions.Item label={<strong>Operating System</strong>}>
+            {data?.osType}
+        </Descriptions.Item>
+    </Descriptions>
+)
 
 export default Widget
