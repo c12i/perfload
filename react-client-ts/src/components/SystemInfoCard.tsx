6af8964c42080dd036c6f750131ca83cabe86162
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-09-14 07:20:48 +0300

    Derive a system descriptions component

--- react-client-ts/src/components/SystemInfoCard.tsx
+++ react-client-ts/src/components/SystemInfoCard.tsx
@@ -1,4 +1,4 @@
-import { Badge, Card, Descriptions } from 'antd'
+import { Badge, Card, Descriptions, Tag } from 'antd'
 import dayjs from 'dayjs'
 import Duration from 'dayjs/plugin/duration'
 import RelativeTime from 'dayjs/plugin/relativeTime'
@@ -15,7 +15,7 @@ const Widget: React.FC<Props> = ({ data }) => {
     return (
         <Badge.Ribbon text={`${data?.osType} Machine`}>
             <Card style={{ width: 900 }}>
-                <Card.Grid hoverable={false} style={{ width: '40%' }}>
+                <Card.Grid hoverable={false} style={{ width: '50%' }}>
                     <h3>CPU Load</h3>
                     <Gauge percent={data?.cpuLoad} />
                     <SystemDescriptions data={data} />
@@ -25,24 +25,33 @@ const Widget: React.FC<Props> = ({ data }) => {
     )
 }
 
-const SystemDescriptions: React.FC<Props> = ({ data }) => (
-    <Descriptions title="CPU Info" layout="vertical">
-        <Descriptions.Item label={<strong>CPU Model</strong>}>
-            {data?.cpuModel}
-        </Descriptions.Item>
-        <Descriptions.Item label={<strong>Speed</strong>}>
-            {data?.cpuSpeed}
-        </Descriptions.Item>
-        <Descriptions.Item label={<strong>Cores</strong>}>
-            {data?.cpuCoreCount}
-        </Descriptions.Item>
-        <Descriptions.Item label={<strong>Uptime</strong>}>
-            {dayjs.duration(data?.uptime, 'seconds').humanize()}
-        </Descriptions.Item>
-        <Descriptions.Item label={<strong>Operating System</strong>}>
-            {data?.osType}
-        </Descriptions.Item>
-    </Descriptions>
-)
+const SystemDescriptions: React.FC<Props> = ({ data }) => {
+    return (
+        <Descriptions title="CPU Info" layout="vertical">
+            <Descriptions.Item label={<strong>CPU Model</strong>}>
+                {data?.cpuModel}
+            </Descriptions.Item>
+            <Descriptions.Item label={<strong>Speed</strong>}>
+                {data?.cpuSpeed}
+            </Descriptions.Item>
+            <Descriptions.Item label={<strong>Cores</strong>}>
+                {data?.cpuCoreCount}
+            </Descriptions.Item>
+            <Descriptions.Item label={<strong>Uptime</strong>}>
+                {dayjs.duration(data?.uptime, 'seconds').humanize()}
+            </Descriptions.Item>
+            <Descriptions.Item label={<strong>Operating System</strong>}>
+                {data?.osType}
+            </Descriptions.Item>
+            <Descriptions.Item label={<strong>Device Status</strong>}>
+                {data?.isOnline ? (
+                    <Tag color="success">Online</Tag>
+                ) : (
+                    <Tag color="default">Offline</Tag>
+                )}
+            </Descriptions.Item>
+        </Descriptions>
+    )
+}
 
 export default Widget
