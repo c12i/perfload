135e06bc18fae9aaa711ab4f2c46c836923b0b5e
Collins Muriuki <hello@collinsmuriuki.xyz>
2021-09-28 22:56:39 +0300

    Update use socket hook

--- react-client-ts/src/hooks/useSocket.ts
+++ react-client-ts/src/hooks/useSocket.ts
@@ -3,13 +3,13 @@ import { useMemo } from 'react'
 
 const { REACT_APP_SERVER_URL } = process.env
 
-const useSocket = (namespace?: string) => {
+const useSocket = (namespace: string = '/') => {
   const socket = useMemo(() => {
-    const socket = io(REACT_APP_SERVER_URL + (namespace || '/'))
+    const socket = io(`${REACT_APP_SERVER_URL}${namespace}`)
     // TODO: secure client-auth key in env vars
     socket.emit('client-auth', '1234abcd')
     return socket
-  }, [])
+  }, [namespace])
   return socket
 }
 
